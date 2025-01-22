cbb06cb687838f2521476a0630f8624ea8e58d4c
ivanSaichkin <ivan.saichkin@bk.ru>
2025-01-22 16:17:20 +0300

    add test file

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Vector.hpp"
+
+int main() {
+    Vector<int> vec;
+    vec.pushBack(1);
+    vec.pushBack(2);
+    vec.pushBack(3);
+
+    std::cout << "Vector elements: ";
+    for (size_t i = 0; i < vec.size(); ++i) {
+        std::cout << vec[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
